eeb695d565a206a13c947cf6d2bc4db926d1fc9a
meansholee <meansholee@gmail.com>
2025-12-28 21:46:15 +0800

    lmx

--- newone/Assets/SpaceFusion/SF Grid Building System/Scripts/Interfaces/IPlacementState.cs
+++ newone/Assets/SpaceFusion/SF Grid Building System/Scripts/Interfaces/IPlacementState.cs
@@ -1,38 +1,26 @@
 ﻿using UnityEngine;
 
-namespace SpaceFusion.SF_Grid_Building_System.Scripts.Interfaces
-{
-    public interface IPlacementState
-    {
-
+namespace SpaceFusion.SF_Grid_Building_System.Scripts.Interfaces {
+    public interface IPlacementState {
+        
         /// <summary>
-        /// 退出状态时的清理工作
+        /// State finished --> handle exit
         /// </summary>
         void EndState();
 
         /// <summary>
-        /// 点击屏幕时触发（现在只用于移动虚影，不要在这里放置！）
+        /// handles actions on mouse button click
         /// </summary>
         void OnAction(Vector3Int gridPosition);
 
         /// <summary>
-        /// [新增] 点击 UI 的“确认”按钮时触发（真正保存数据/放置物体）
-        /// </summary>
-        void OnConfirm();
-
-        /// <summary>
-        /// [新增] 点击 UI 的“取消”按钮时触发
-        /// </summary>
-        void OnCancel();
-
-        /// <summary>
-        /// 更新状态（每帧调用，用于刷新虚影位置）
+        /// Updates the current state: e.g. grid cell changed etc.
         /// </summary>
         void UpdateState(Vector3Int gridPosition);
 
         /// <summary>
-        /// 点击 UI 的“旋转”按钮时触发
+        /// triggered when player rotates an object
         /// </summary>
-        void OnRotation();
+        public void OnRotation();
     }
 }
--- newone/Assets/SpaceFusion/SF Grid Building System/Scripts/PlacementStates/RemoveState.cs
+++ newone/Assets/SpaceFusion/SF Grid Building System/Scripts/PlacementStates/RemoveState.cs
@@ -2,19 +2,20 @@
 using SpaceFusion.SF_Grid_Building_System.Scripts.Interfaces;
 using UnityEngine;
 
-namespace SpaceFusion.SF_Grid_Building_System.Scripts.PlacementStates
-{
-    public class RemoveState : IPlacementState
-    {
+namespace SpaceFusion.SF_Grid_Building_System.Scripts.PlacementStates {
+    
+    /// <summary>
+    /// removes a object on the selected position for the selected GridData
+    /// </summary>
+    public class RemoveState : IPlacementState {
         private string _guid;
         private readonly IPlacementGrid _grid;
         private readonly PreviewSystem _previewSystem;
         private readonly GridData _gridData;
         private readonly PlacementHandler _placementHandler;
-        private Vector3Int _targetPos;
 
-        public RemoveState(IPlacementGrid grid, PreviewSystem previewSystem, GridData gridData, PlacementHandler placementHandler)
-        {
+
+        public RemoveState(IPlacementGrid grid, PreviewSystem previewSystem, GridData gridData, PlacementHandler placementHandler) {
             _grid = grid;
             _previewSystem = previewSystem;
             _gridData = gridData;
@@ -22,36 +23,39 @@ namespace SpaceFusion.SF_Grid_Building_System.Scripts.PlacementStates
             previewSystem.StartShowingRemovePreview(_grid.CellSize);
         }
 
-        public void EndState()
-        {
+
+        public void EndState() {
             _previewSystem.StopShowingPreview();
         }
 
-        public void OnAction(Vector3Int gridPosition)
-        {
-            // 只是选中这个格子，显示红框
-            _targetPos = gridPosition;
-            UpdateState(gridPosition);
-        }
-
-        public void OnConfirm()
-        {
-            // 真正的删除逻辑在这里执行
-            _guid = _gridData.GetGuid(_targetPos);
-            if (_guid != null)
-            {
-                _gridData.RemoveObjectPositions(_targetPos);
-                _placementHandler.RemoveObjectPositions(_guid);
+        public void OnAction(Vector3Int gridPosition) {
+            _guid = _gridData.GetGuid(gridPosition);
+            if (_guid == null) {
+                Debug.LogWarning($"Remove action: Nothing to remove on grid position {gridPosition}");
+                return;
             }
+
+            // free the positions from the grid
+            _gridData.RemoveObjectPositions(gridPosition);
+            _placementHandler.RemoveObjectPositions(_guid);
+
+            var cellPosition = _grid.CellToWorld(gridPosition);
+            // return NEGATED empty position for the Update method
+            // since we want to remove an object, but the position is empty, our validity is false...
+            _previewSystem.UpdateRemovalPosition(cellPosition, !IsPositionEmpty(gridPosition));
         }
 
-        public void OnCancel() { }
-        public void OnRotation() { }
+        private bool IsPositionEmpty(Vector3Int gridPosition) {
+            return _gridData.IsPlaceable(gridPosition, Vector2Int.one);
+        }
 
-        public void UpdateState(Vector3Int gridPosition)
-        {
-            var validity = !(_gridData.IsPlaceable(gridPosition, Vector2Int.one));
+        public void UpdateState(Vector3Int gridPosition) {
+            var validity = !IsPositionEmpty(gridPosition);
             _previewSystem.UpdateRemovalPosition(_grid.CellToWorld(gridPosition), validity);
+        }
+
+        public void OnRotation() {
+            // Do nothing since we only want to remove
         }
     }
 }
